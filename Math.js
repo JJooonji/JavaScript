32f2f7afb97846f4eebaf8a4a27f0fded56eea7b
JJooonji <korewa9@naver.com>
2022-07-18 19:29:42 +0900

    Update Math.js

--- Math.js
+++ Math.js
@@ -58,5 +58,21 @@ console.log(Number(sum.toFixed(1)))//숫자형으로 변환
 console.log(+sum.toFixed(1))//숫자형으로 변환
 console.log(Math.round(sum*10)/10)//필요한 소수점 자리수만큼 10의 거듭제곱을 하여 정수로 먼저 만들어 round 메소드로 계산을 한 후 다시 그만큼 나눠주어 계산
 
+//실습과제(이자계산기)
+// 여기에 코드를 작성해 주세요.
 
-[다양한 Math 함수 보기:https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math]
+function interestCalculator(rate, payment, term){
+    let result = payment * term * (term + 1) / 2 * rate / 12
+    console.log(result.toFixed(0))
+}
+
+// // 이율이 4.3%일 때 매월 80만원씩 24개월 납입할 경우
+interestCalculator(0.043, 800000, 24);
+
+// // 이율이 4.3%일 때 매월 60만원씩 24개월 납입할 경우
+interestCalculator(0.043, 600000, 24);
+
+
+
+
+// 다양한 Math 함수 보기:https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math
